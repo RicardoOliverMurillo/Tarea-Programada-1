493f8158cc45f8680dd8ec0dbe7b55e174b07eba
ricardo <ricardo@ricardo-PC>
2018-09-06 16:27:28 -0600

    prueba

---
+++ TareaProgramada1/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+
+typedef struct doctor{
+    char name[10];
+    int age;
+    int id_code;
+}
+
+
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
