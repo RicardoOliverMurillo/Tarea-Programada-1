2225ce662db9474e305dfbe26f3aa2da0869c800
Cartago <Cartago@LAPTOP-HC09RG3L>
2018-09-08 10:20:09 -0600

    Función de menú principal y los direferentes case que pueden haber.

--- TareaProgramada1/main.c
+++ TareaProgramada1/main.c
@@ -1,19 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
+/*Función que activa el menú principal*/
+/*Entradas: no cuenta con entradas principales, dentro de la función se solicita el número de opción que desea ejecutar*/
+/*Salidas: menú principal de la aplicación, ejecuta funciones depende de la opción seleccionada*/
 
-/*Anjelica es una intensa
- y necia */
-
-typedef struct doctor{
-    char name[10];
-    int age;
-    int id_code;
+void imprimirMenu(){
+        
+    bool bandera = true;
+    int opcionSeleccionada;
+    int opcionEstadistica = 0;
+    
+    while(bandera == true){
+        printf("###############################################################\n");
+        printf("Bienvenido... Seleccione alguna de las siguientes opciones: \n");
+        printf("1. Cargar datos \n2. Resgistar nuevo médico \n3. Programar citas \n4. Estadísticas \n5. Salir \n");
+        printf("\nOpción seleccionada:");
+        scanf("%d", &opcionSeleccionada);
+        printf("###############################################################\n");
+        
+        switch(opcionSeleccionada){
+            case 1:
+                printf("###");
+                break;
+            case 2:
+                printf("###");
+                break;
+            case 3:
+                printf("###");
+                break;
+            case 4:
+                printf("Seleccione la opción que desea ver: \n"
+                "1. Médico con más citas y promedio general de citas\n 2. Especialidad con más citas y promedio de citas"
+                        "por especialidad\n 3. Promedio de citas por paciente\n");
+                scanf("d%", &opcionEstadistica);
+                break;
+            case 5:
+                bandera = false;
+                break;
+        }
+    }
 }
 
 
 int main(int argc, char** argv) {
-
-    return (EXIT_SUCCESS);
+    imprimirMenu();
+    return 0;
 }
 
