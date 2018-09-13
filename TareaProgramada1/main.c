0501c0da5038e4e6721e27694c61e66d3ad73aa5
Cartago <Cartago@LAPTOP-HC09RG3L>
2018-09-13 17:28:34 -0600

    Merge origin/master
    Conflicts:
    	TareaProgramada1/main.c

--- TareaProgramada1/main.c
+++ TareaProgramada1/main.c
@@ -3,8 +3,10 @@
 #include <stdbool.h>
 #include <string.h>
 
+/*ESTRUCTURAS*/
+
 typedef struct doctor{
-    int id_medico;
+    char id_medico[30];
     char nombre[30];
     char primer_apellido[30];
     char especialidad[30];
@@ -14,10 +16,10 @@ typedef struct doctor{
 Doctor* cabezaDoctor = NULL;
 
 typedef struct paciente{
-    int id_paciente;
+    char id_paciente[30];
     char nombre[30];
     char primer_apellido[30];
-    int edad; /*edad debe ser en años*/
+    char edad[30]; /*edad debe ser en años*/
     char telefono[30]; /*formato debe ser ####-####*/
     struct paciente* sig;
 } Paciente;
@@ -44,6 +46,7 @@ typedef struct cita{
 }Cita;
 Cita* cabezaCita = NULL;
 
+
 /*Función que agrega un nuevo doctor a la lista de doctores*/
 /*Entradas: no contiene entradas*/
 /*Salidas: se almacena el nuevo doctor en la lista*/
@@ -52,7 +55,7 @@ void agregarDoctor(){
     char primerApellido[30];
     char especialidadDoctor[30];
     char turno[10];
-    int id_doctor;
+    char id_doctor[30];
     
     Doctor* nuevoDoctor = (Doctor*)malloc(sizeof(Doctor));
     
@@ -65,22 +68,22 @@ void agregarDoctor(){
     printf("Inserte el turno del doctor: ");
     scanf("%s", &turno);
     printf("Inserte el id del doctor: ");
-    scanf("%d", &id_doctor);
+    scanf("%s", &id_doctor);
     
     
     strcpy(nuevoDoctor->nombre, nombreDoctor);
     strcpy(nuevoDoctor->primer_apellido, primerApellido);
     strcpy(nuevoDoctor->especialidad, especialidadDoctor);
     strcpy(nuevoDoctor->turno, turno);
-    nuevoDoctor->id_medico = id_doctor;
+    strcpy(nuevoDoctor->id_medico,id_doctor);
     
     if(cabezaDoctor == NULL){
         cabezaDoctor = nuevoDoctor;
         cabezaDoctor->sig = NULL;
     }
     else{
-        nuevoDoctor->sig = cabezaDoctor;
-        cabezaDoctor = nuevoDoctor;
+        cabezaDoctor->sig = nuevoDoctor;
+        nuevoDoctor->sig = NULL;
     }
     printf("El médico se añadió exitosamente...\n");
 }
@@ -92,8 +95,8 @@ void agregarPaciente(){
     char nombrePaciente[30];
     char apellidoPaciente[30];
     char telefono[30];
-    int edad; 
-    int id_paciente;
+    char edad[30]; 
+    char id_paciente[30];
     
     Paciente* nuevoPaciente = (Paciente*)malloc(sizeof(Paciente));
     
@@ -104,15 +107,15 @@ void agregarPaciente(){
     printf("Ingrese el teléfono del paciente (respete el formato ####-####: ");
     scanf("%s", &telefono);
     printf("Ingrese la edad en años del paciente: ");
-    scanf("%d", &edad);
+    scanf("%s", &edad);
     printf("Ingrese el id del paciente: ");
-    scanf("%d", &id_paciente);
+    scanf("%s", &id_paciente);
     
     strcpy(nuevoPaciente->nombre, nombrePaciente);
     strcpy(nuevoPaciente->primer_apellido, apellidoPaciente);
     strcpy(nuevoPaciente->telefono, telefono);
-    nuevoPaciente->edad = edad;
-    nuevoPaciente->id_paciente = id_paciente;
+    strcpy(nuevoPaciente->edad,edad);
+    strcpy(nuevoPaciente->id_paciente,id_paciente);
     /*Se deben validar los formatos de los datos ingresados
      */
     if(cabezaPaciente == NULL){
@@ -125,11 +128,6 @@ void agregarPaciente(){
     }
     printf("El paciente se añadió exitosamente...\n");
 }
-
-/*void buscarDoctor(){
-    Un médico no puede tener dos citas en la misma fecha y hora
-    Un médico no puede tener citas en horas diferentes a las de su turno
-}*/
 
 bool buscarPaciente(char* nombrePaciente, Paciente* ListaPaciente){
     /*Un paciente no puede tener más de una cita en la misma fecha y hora un  
@@ -152,32 +150,41 @@ void estaDoctor(Doctor* tempDoctor, char* nombre){
     
 }
 
+/*Función que almacena los pacientes de la lista en un txt
+ * Entradas: la cabeza de la lista de pacientes
+ * Salidas: los datos almacenados en el txt
+ */
 void guardarPacientes(Paciente* ListaPacientes){
     /*Almacena pacientes en el pacientes.txt*/
     FILE * miarchivo;
     char* nombrearchivo = "pacientes.txt";
     Paciente* temp = ListaPacientes;
+    miarchivo = fopen(nombrearchivo, "w"); //agrega al final de la lista
     
     while(temp != NULL){
-       miarchivo = fopen(nombrearchivo, "a"); //agrega al final de la lista
-        fprintf(miarchivo, "%s, %s, %s, %d, %d\r\n", 
+        fprintf(miarchivo, "%s, %s, %s, %s, %s\r\n", 
             temp->nombre,temp->primer_apellido, temp->telefono, temp->edad, temp->id_paciente);
         temp = temp->sig;
     }
     fclose(miarchivo);
 }
 
+/*Función que almacena los doctorres de la lista en un txt
+ * Entradas: la cabeza de la lista de doctores
+ * Salidas: los datos almacenados en el txt
+ */
 void guardarDoctores(Doctor* ListaDoctores){
     /*Almacena doctores en el doctores.txt*/
     
     FILE* miarchivo;
     char* nombrearchivo = "doctores.txt";
     Doctor* temp = ListaDoctores;
+    miarchivo=fopen(nombrearchivo,"w");
+    
     while(temp != NULL){
-        miarchivo=fopen(nombrearchivo,"a");
-        fprintf(miarchivo, "%s, %s, %s, %s, %d \r\n", 
-            temp->nombre, temp->primer_apellido, temp->especialidad, temp->turno, temp->id_medico);
-        temp = temp->sig;
+            fprintf(miarchivo, "%s, %s, %s, %s, %s \r\n", 
+                temp->nombre, temp->primer_apellido, temp->especialidad, temp->turno, temp->id_medico);
+            temp = temp->sig;
     }
     fclose(miarchivo);
     
@@ -187,16 +194,77 @@ void guardarCitas(Cita* ListaCitas){
     /*Almacena doctores en el citas.txt*/
 }
 
-void separarString(char linea[]){
+/*Funcion que separa la linea de texto del txt correspondiente y devuelve una estructura Doctor
+ Entradas: la linea de texto
+ Salida: estructura Doctor*/
+
+Doctor* separarStringDoctor(char linea[]){
+    char* nombreDoctor;
+    char* primerApellido;
+    char* especialidadDoctor;
+    char* turno;
+    char* id_doctor;
+    
+    Doctor* nuevoDoctor = (Doctor*)malloc(sizeof(Doctor));
+    
     char * pch;
     pch = strtok (linea," ,.-");
+    int contador = 0;
+    
     while (pch != NULL)
     {
-        printf ("%s\n",pch);
+        switch(contador){
+            case 0:
+                nombreDoctor = pch;
+                strcpy(nuevoDoctor->nombre, nombreDoctor);
+                //printf("%s\n", nombreDoctor);
+            case 1:
+                primerApellido = pch;
+                strcpy(nuevoDoctor->primer_apellido, primerApellido);
+            case 2:
+                especialidadDoctor = pch;
+                strcpy(nuevoDoctor->especialidad, especialidadDoctor);
+            case 3:
+                turno = pch;
+                strcpy(nuevoDoctor->turno, turno);
+            case 4:
+                id_doctor = pch;
+                strcpy(nuevoDoctor->id_medico, id_doctor);
+              
+        }
+        contador = contador+1;
+        //printf ("%s\n",pch);
         pch = strtok (NULL, " ,.-");
+        
+    }
+    return nuevoDoctor;
+}
+
+/*Función que imprime la lista doctores
+ */
+void imprimirLista(Doctor* ptr){
+    while (ptr != NULL){
+        printf("El nombre del doctor es %s, %s, %s, %s, %s\n", ptr->nombre, ptr->primer_apellido, ptr->especialidad,
+                ptr->turno, ptr->id_medico);
+        ptr = ptr ->sig;
     }
 }
-void cargarDatos(){
+
+/*Función que imprime la lista pacientes
+ */
+void imprimirListaPaciente(Paciente* ptr){
+    while (ptr != NULL){
+        printf("El nombre del paciente es %s, %s, %s, %s, %s\n", ptr->nombre, ptr->primer_apellido, ptr->telefono,
+                ptr->edad, ptr->id_paciente);
+        ptr = ptr ->sig;
+    }
+}
+
+/*Función que lee el txt y carga los datos en memoria
+ * Entradas: no tiene entradas
+ * Salidas: los datos cargados en memoria
+ */
+void cargarDatosDoctor(){
     FILE *miarchivo;
     char linea[256];
     char *resultado;
@@ -209,11 +277,101 @@ void cargarDatos(){
             resultado = fgets(linea, 256, miarchivo);
 
             while (resultado != NULL) {
-                    printf("%s", linea);
-                    separarString(linea);
+                    //printf("%s", linea);
+                    Doctor* doc = separarStringDoctor(linea);
+                    
+                    if(cabezaDoctor == NULL){
+                        cabezaDoctor = doc;
+                        cabezaDoctor->sig = NULL;
+                    }
+                    else{
+                        doc->sig = cabezaDoctor;
+                        cabezaDoctor = doc;
+                    }
                     resultado = fgets(linea, 256, miarchivo);
             }
             fclose(miarchivo);
+            imprimirLista(cabezaDoctor);
+    }
+}
+
+/*Funcion que separa la linea de texto del txt correspondiente y devuelve una estructura Paciente
+ Entradas: la linea de texto
+ Salida: estructura Paciente*/
+
+Paciente* separarStringPaciente(char linea[]){
+    char* nombrePaciente;
+    char* apellidoPaciente;
+    char* telefono;
+    char* edad; 
+    char* id_paciente;
+    
+    Paciente* nuevoPaciente = (Paciente*)malloc(sizeof(Paciente));
+    
+    char * pch;
+    pch = strtok (linea," ,.-");
+    int contador = 0;
+    
+    while (pch != NULL)
+    {
+        switch(contador){
+            case 0:
+                nombrePaciente = pch;
+                strcpy(nuevoPaciente->nombre, nombrePaciente);
+            case 1:
+                apellidoPaciente = pch;
+                strcpy(nuevoPaciente->primer_apellido, apellidoPaciente);
+            case 2:
+                telefono = pch;
+                strcpy(nuevoPaciente->telefono, telefono);
+            case 3:
+                edad = pch;
+                strcpy(nuevoPaciente->edad, edad);
+            case 4:
+                id_paciente = pch;
+                strcpy(nuevoPaciente->id_paciente,id_paciente);
+              
+        }
+        contador = contador+1;
+        //printf ("%s\n",pch);
+        pch = strtok (NULL, " ,.-");
+        
+    }
+    return nuevoPaciente;
+}
+
+/*Función que lee el txt y carga los datos en memoria
+ * Entradas: no tiene entradas
+ * Salidas: los datos cargados en memoria
+ */
+void cargarDatosPaciente(){
+    FILE *miarchivo;
+    char linea[256];
+    char *resultado;
+
+    miarchivo = fopen("pacientes.txt", "rt");
+
+    if (miarchivo == NULL) {
+            printf("Error: No se ha podido crear el fichero doctores.txt");
+    } else {
+            resultado = fgets(linea, 256, miarchivo);
+
+            while (resultado != NULL) {
+                    //printf("%s", linea);
+                    Paciente* paciente = separarStringPaciente(linea);
+                    
+                    if(cabezaPaciente == NULL){
+                        cabezaPaciente = paciente;
+                        cabezaPaciente->sig = NULL;
+                    }
+                    else{
+                        paciente->sig = cabezaPaciente;
+                        cabezaPaciente = paciente;
+                    }
+                    resultado = fgets(linea, 256, miarchivo);
+            }
+            fclose(miarchivo);
+            imprimirListaPaciente(cabezaPaciente);
     }
 }
 
@@ -238,7 +396,6 @@ void calcularPromedioPacientes(){
 /*Función que activa el menú principal*/
 /*Entradas: no cuenta con entradas principales, dentro de la función se solicita el número de opción que desea ejecutar*/
 /*Salidas: menú principal de la aplicación, ejecuta funciones depende de la opción seleccionada*/
-
 void imprimirMenu(){
     bool bandera = true;
     int opcionSeleccionada;
@@ -254,13 +411,14 @@ void imprimirMenu(){
         
         switch(opcionSeleccionada){
             case 1:
-                cargarDatos();
+                cargarDatosDoctor();
+                cargarDatosPaciente();
                 break;
             case 2:
                 agregarDoctor();
                 break;
             case 3:
-                agregarCita();
+                //agregarPaciente();
                 break;
             case 4:
                 printf("Seleccione la opción que desea ver: \n"
